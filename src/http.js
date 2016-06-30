bf4fa3834c4878900bf61d7a9d8ec2e1f8f6abeb
Vijay Rangan <vijay.rangan@gmail.com>
2016-06-30 10:59:13 +0530

    add 'PUT' http method. improve docs.

--- src/http.js
+++ src/http.js
@@ -4,9 +4,36 @@ let defaults = {
   },
 
   options: {
+    cache: 'default',
+    credentials: 'include',
     mode: 'cors'
   }
 };
+
+
+/**
+ * Build custom headers.
+ *
+ * @param  {Object} headers User provided headers
+ * @return {Object}         Mixed object from defaults and user provided headers.
+ */
+var _buildHeaders = function(headers) {
+  let build = Object.assign(defaults.headers, headers)
+
+  return build;
+}
+
+var _buildRequest = function(uri, method, data, headers, options) {
+  var options = options || {};
+  return new Request(uri, {
+    method: method,
+    data: data ? JSON.stringify(data) : '',
+    mode: options.mode || defaults.options.mode,
+    cache: options.cache || defaults.options.cache,
+    credentials: options.credentials || defaults.options.credentials,
+    headers: new Headers(headers)
+  });
+}
 
 /**
  * HTTP service for easy AJAX calls
@@ -23,13 +50,8 @@ module.exports = {
    * @return {Promise} Response data in json format.
    */
   get: function(uri, headers = {}, options = {}) {
-    let customHeaders = Object.assign(defaults.headers, headers)
-
-    var req = new Request(uri, {
-      method: 'GET',
-      mode: options.mode || defaults.options.mode,
-      headers: new Headers(customHeaders)
-    });
+    let customHeaders = _buildHeaders(headers),
+        req = _buildRequest(uri, 'GET', customHeaders, options)
 
 
     return new Promise((resolve, reject) => {
@@ -50,20 +72,34 @@ module.exports = {
    * @return {Promise} Response data in json format.
    */
   post: function(uri, data, headers = {}, options = {}) {
-      let customHeaders = Object.assign(defaults.headers, headers)
-
-      var req = new Request(uri, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        mode: options.mode || defaults.options.mode,
-        headers: new Headers(customHeaders)
-      })
-
+      let customHeaders = _buildHeaders(headers),
+          req = _buildRequest(uri, 'POST', data, customHeaders, options);
 
       return new Promise((resolve, reject) => {
         fetch(req)
           .then(response => resolve(response.json()))
           .catch(err => reject(err))
       });
-  }
+  },
+
+  /**
+   * HTTP 'PUT' request
+   *
+   * @param  {String} uri
+   * @param  {Object} data
+   * @param  {Object} headers
+   * @param  {Object} options
+   *
+   * @return {Promise} Response data in json format.
+   */
+  put: function(uri, data, headers = {}, options = {}) {
+    let customHeaders = _buildHeaders(headers),
+        req = _buildRequest(uri, 'PUT', data, customHeaders, options)
+
+      return new Promise((resolve, reject) => {
+        fetch(req)
+          .then(response => resolve(response.json()))
+          .catch(error => reject(error));
+      });
+    }
 };
